bcca28474fb4e780d5afc09cb7b002d65d749cb1
Fatima Choudhury <f.choudhury2382@student.leedsbeckett.ac.uk>
2025-05-23 04:45:38 +0100

    slay

--- AssignmentY2/taxi.py
+++ AssignmentY2/taxi.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 taxi_df = pd.read_csv('TaxiRideShare .csv', index_col=0)
 print(taxi_df.head())
@@ -28,3 +31,30 @@ print(taxi_df.duplicated() .sum())
 taxi_df.drop_duplicates(inplace=True)
 print("\nTotal number of duplicates")
 print(taxi_df.duplicated() .sum())
+print(mpl.__version__)
+
+x = taxi_df['hour']
+y = taxi_df['price']
+plt.title("time vs price scatter plot")
+plt.xlabel("hour")
+plt.ylabel("price")
+plt.scatter(x, y, color='hotpink')
+plt.show()
+
+x=taxi_df['hour']
+plt.hist(x)
+plt.title("taxi journeys per hour")
+plt.xlabel("hour")
+plt.ylabel("count")
+plt.show()
+
+sns.lineplot(x=taxi_df['hour'], y=taxi_df['price'], data=taxi_df)
+plt.show()
+
+x = taxi_df['distance']
+y = taxi_df['price']
+plt.title("distance vs price scatter plot")
+plt.xlabel("distance")
+plt.ylabel("price")
+plt.scatter(x, y, color='red')
+plt.show()

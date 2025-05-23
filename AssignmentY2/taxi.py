4c9eb3147a9a354bafe0932db431b5d08dc2a221
Fatima Choudhury <f.choudhury2382@student.leedsbeckett.ac.uk>
2025-05-23 08:07:09 +0100

    slay

--- AssignmentY2/taxi.py
+++ AssignmentY2/taxi.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
+from numpy.ma.core import count
 
 taxi_df = pd.read_csv('TaxiRideShare .csv', index_col=0)
 print(taxi_df.head())
@@ -21,6 +22,7 @@ print(taxi_df.duplicated() .sum())
 # removing missing values
 taxi_df.drop(['timestamp'], axis=1, inplace=True)
 taxi_df.drop(['datetime'], axis=1, inplace=True)
+taxi_df.drop(['visibility.1'], axis=1, inplace=True)
 taxi_df.dropna(inplace=True)
 print(taxi_df.isna().sum())
 print(taxi_df.info)
@@ -33,23 +35,73 @@ print("\nTotal number of duplicates")
 print(taxi_df.duplicated() .sum())
 print(mpl.__version__)
 
+#c = np.corrcoef(taxi_df['price'], taxi_df['hour'])
+#print('Correlation between price and time\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['day'])
+print('Correlation between price and day\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['month'])
+print('Correlation between price and month\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['distance'])
+print('Correlation between price and distance\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['surge_multiplier'])
+print('Correlation between price and surge multiplier\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['temperature'])
+print('Correlation between price and temp\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['apparentTemperature'])
+print('Correlation between price and apparent temp\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['precipIntensity'])
+print('Correlation between price and precip intensity\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['precipProbability'])
+print('Correlation between price and precip probability\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['humidity'])
+print('Correlation between price and humidity\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['windSpeed'])
+print('Correlation between price and wind speed\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['windGust'])
+print('Correlation between price and wind Gust\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['visibility'])
+print('Correlation between price and visibilty\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['temperatureHigh'])
+print('Correlation between price and temp high\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['temperatureLow'])
+print('Correlation between price and temp low\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['dewPoint'])
+print('Correlation between price and dew\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['pressure'])
+print('Correlation between price and pressure\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['windBearing'])
+print('Correlation between price and windBearing\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['cloudCover'])
+print('Correlation between price and clouds\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['uvIndex'])
+print('Correlation between price and UV\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['moonPhase'])
+print('Correlation between price and moon\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['precipIntensityMax'])
+print('Correlation between price and PIM\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['temperatureMin'])
+print('Correlation between price and temp min\n',c)
+c = np.corrcoef(taxi_df['price'], taxi_df['temperatureMax'])
+print('Correlation between price and temp max\n',c)
+
+
 x = taxi_df['hour']
 y = taxi_df['price']
 plt.title("time vs price scatter plot")
 plt.xlabel("hour")
 plt.ylabel("price")
 plt.scatter(x, y, color='hotpink')
-plt.show()
+#plt.show()
 
 x=taxi_df['hour']
 plt.hist(x)
 plt.title("taxi journeys per hour")
 plt.xlabel("hour")
 plt.ylabel("count")
-plt.show()
+#plt.show()
 
 sns.lineplot(x=taxi_df['hour'], y=taxi_df['price'], data=taxi_df)
-plt.show()
+#plt.show()
 
 x = taxi_df['distance']
 y = taxi_df['price']
@@ -57,4 +109,37 @@ plt.title("distance vs price scatter plot")
 plt.xlabel("distance")
 plt.ylabel("price")
 plt.scatter(x, y, color='red')
-plt.show()
+#plt.show()
+
+x = taxi_df['surge_multiplier']
+y = taxi_df['price']
+plt.title("surge multiplier vs price scatter plot")
+plt.xlabel("surge multiplier")
+plt.ylabel("price")
+plt.scatter(x, y, color='red')
+#plt.show()
+pd.set_option('display.max_columns', None)
+print("Mode values:")
+print(taxi_df.mode())
+print("\n price standard deviation:")
+print(taxi_df['price'].std())
+print("\n distance standard deviation:")
+print(taxi_df['distance'].std())
+print("\n surge multiplier standard deviation:")
+print(taxi_df['surge_multiplier'].std())
+print("\n price skewness:")
+print(taxi_df['price'].skew())
+print("\n distance skewness:")
+print(taxi_df['distance'].skew())
+print("\n surge_multiplier skewness:")
+print(taxi_df['surge_multiplier'].skew())
+
+#seperate dependent and independent values
+#independent variables
+x = taxi_df.loc[:,['distance', 'surge_multiplier']].values
+#print(x)
+#dependent variable
+y = taxi_df.loc[:,['price']].values
+#print(y)
+#splitting dataset into training and testing set
+from sklearn.model_selection import train_test_split
